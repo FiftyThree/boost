a1f9f2b465969c334d9072c176d076250c12637b
Max Liberman <max@fiftythree.com>
2016-04-11 23:36:52 -0400

    Progress on refactoring build script

--- build.py
+++ build.py
@@ -8,10 +8,27 @@ import subprocess
 import sys
 import traceback
 
-VERBOSE = False
+VERBOSE = True
 
-BOOST_LIBS     = ('chrono', 'thread', 'system')
-BOOST_HEADERS  = (
+# Compiler
+COMPILER   = 'clang++'
+CPP_STD    = 'c++14'
+STD_LIB    = 'libc++'
+
+# Operating Systems
+IOS_MIN_VERSION = '8.0'
+OSX_MIN_VERSION = '10.10'
+
+# Architectures
+ARCHITECTURES = {
+    'ios': ('armv7', 'arm64'),
+    'simulator': ('i386', 'x86_64'),
+    'osx': ('i386', 'x86_64')
+}
+
+BOOST_VERSION  = '1.60.0'
+BOOST_LIBS     = ['chrono', 'thread', 'system']
+BOOST_HEADERS  = [
     'algorithm',
     'any',
     'exception',
@@ -24,7 +41,9 @@ BOOST_HEADERS  = (
     'boost/container/flat_set.hpp',
     'boost/scope_exit.hpp',
     'boost/uuid/sha1.hpp'
-    )
+    ]
+
+BOOST_TARBALL_URL_TEMPLATE = 'http://sourceforge.net/projects/boost/files/boost/{}/boost_{}.tar.bz2/download'
 
 #-------------------------------------------------------------------------------
 #
@@ -112,7 +131,7 @@ def shell_pipe(callback, cmd, redirect_stderr_to_stdout = True):
 #
 #-------------------------------------------------------------------------------
 
-IOS_MIN_VERSION            = '8.0'
+
 XCODE_ROOT                 = shell_output('xcode-select -print-path')
 IOS_SDK_VERSION            = shell_output('xcrun -sdk iphoneos --show-sdk-version')
 OSX_SDK_VERSION            = shell_output('xcrun -sdk macosx --show-sdk-version')
@@ -137,9 +156,6 @@ BUILD_IOS_LIB_DIR          = os.path.join(BUILD_DIR, 'libs/boost/lib')
 BUILD_IOS_INCLUDE_DIR      = os.path.join(BUILD_DIR, 'libs/boost/include/boost')
 BUILD_IOS_PREFIX_DIR       = os.path.join(BUILD_DIR, 'ios/prefix')
 
-COMPILER                   = 'clang++'
-CPP_STD                    = 'c++14'
-STD_LIB                    = 'libc++'
 NUM_THREADS                = 16
 CPP_FLAGS                  = ' '.join((
                             '-fPIC', 
@@ -183,75 +199,253 @@ using darwin : {ios_sdk_version}~iphonesim
 #
 #-------------------------------------------------------------------------------
 
-def clean():
-    print 'Cleaning up {}'.format(BUILD_DIR)
-    if (os.path.isdir(BUILD_DIR)):
-        shutil.rmtree(BUILD_DIR)
+class BuildEnv:
+    def __init__(self, root):
+        self.root = root
+        self.dir_stack = []
+        self.xcode_root = shell_output('xcode-select -print-path')
+        self.ios_sdk_version = shell_output('xcrun -sdk iphoneos --show-sdk-version')
+        self.osx_sdk_version = shell_output('xcrun -sdk macosx --show-sdk-version')
+        self.ios_simulator_root = os.path.join(self.xcode_root, 'Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator{}.sdk'.format(self.ios_sdk_version))
 
-def download_boost_tarball():
-    if (os.path.isfile(BOOST_TARBALL_PATH)):
-        print 'Found Boost {} tarball: {}'.format(BOOST_VERSION_PERIOD, BOOST_TARBALL_PATH)
-    else:
-        print 'Downloading Boost {} tarball: {}'.format(BOOST_VERSION_PERIOD, BOOST_TARBALL_PATH)
-        shell('curl -L -o {} {}'.format(BOOST_TARBALL_PATH, BOOST_TARBALL_URL))
+    def resolve_path(self, relative_path):
+        return os.path.join(self.root, relative_path)
 
-def unpack_boost_tarball():
-    print 'Unpacking tarball in {}'.format(BOOST_ROOT)
-    cwd = os.getcwd()
-    os.chdir(BUILD_DIR)
-    shell('tar xfj {}'.format(BOOST_TARBALL_PATH))
-    os.chdir(cwd)
+    def make_dir(self, relative_path):
+        path = self.resolve_path(relative_path)
+        if not os.path.isdir(path):
+            print 'Creating {}'.format(path)
+            os.makedirs(path)
+        return path
 
-def invent_missing_headers():
+    def prepare(self):
+        self.make_dir('')
+
+    def clean(self):
+        print 'Cleaning up {}'.format(self.root)
+        if (os.path.isdir(self.root)):
+            shutil.rmtree(self.root)
+
+    def cd(self, path):
+        print 'cd {}'.format(path)
+        os.chdir(path)
+
+    def push_dir(self, relative_path):
+        self.dir_stack.append(os.getcwd())
+        self.cd(self.resolve_path(relative_path))
+
+    def pop_dir(self):
+        if len(self.dir_stack) > 0:
+            directory = self.dir_stack.pop()
+            self.cd(directory)
+
+class BoostSource:
+    def __init__(self, build_env, version):
+        self.build_env = build_env
+        self.version = version
+        self.version_underscore = '_'.join(self.version.split('.'))
+        self.root = self.build_env.resolve_path('boost_{}'.format(self.version_underscore))
+        self.tarball_url = BOOST_TARBALL_URL_TEMPLATE.format(self.version, self.version_underscore)
+        self.tarball_path = os.path.join(self.build_env.root, 'boost_{}.tar.bz2'.format(self.version_underscore))
+        self.config_path = os.path.join(self.root, 'user-config.jam')
+
+    def resolve_path(self, relative_path):
+        return os.path.join(self.root, relative_path)
+
+    def download(self):
+        if os.path.isfile(self.tarball_path):
+            print 'Found Boost {} tarball: {}'.format(self.version, self.tarball_path)
+        else:
+            print 'Downloading Boost {} tarball: {}'.format(self.version, self.tarball_path)
+            shell('curl -L -o {} {}'.format(self.tarball_path, self.tarball_url))
+
+    def unpack(self):
+        if not os.path.isfile(self.tarball_path):
+            print 'Boost {} tarball not found: {}'.format(self.version, self.tarball_path)
+        if os.path.isdir(self.root):
+            print 'Found Boost {} source: {}'.format(self.version, self.root)
+        else:
+            self.build_env.make_dir(self.root)
+            self.build_env.push_dir(self.build_env.root)
+            print 'Unpacking Boost {} tarball in {}'.format(self.version, self.build_env.root)
+            shell('tar xfj {}'.format(self.tarball_path))
+            self.build_env.pop_dir()
+
     # These files are missing in the ARM iPhoneOS SDK, but they are in the simulator.
     # They are supported on the device, so we copy them from x86 SDK to a staging area
     # to use them on ARM, too.
-    include_dir = os.path.join(IOS_SIMULATOR_ROOT, 'usr/include')
-    for filename in ['crt_externs.h', 'bzlib.h']:
-        filepath = os.path.join(include_dir, filename)
-        if not os.path.isfile(filepath):
-            print 'File doesn\'t exist: {}'.format(filepath)
-            sys.exit(1)
-        print 'Copying {} to {}'.format(filepath, os.path.join(BOOST_ROOT, filename))
-        shutil.copyfile(filepath, os.path.join(BOOST_ROOT, filename))
+    def invent_missing_headers(self):
+        simulator_include = os.path.join(self.build_env.ios_simulator_root, 'usr/include')
+        for filename in ['crt_externs.h', 'bzlib.h']:
+            filepath = os.path.join(simulator_include, filename)
+            if not os.path.isfile(filepath):
+                print 'File doesn\'t exist: {}'.format(filepath)
+                sys.exit(1)
+            dst_path = self.resolve_path(filename)
+            if not os.path.isfile(dst_path):
+                print 'Copying {}'.format(filename)
+                shutil.copyfile(filepath, dst_path)
 
-def make_dir(path):
-    if not os.path.isdir(path):
-        os.makedirs(path)
+    def bootstrap(self):
+        self.build_env.push_dir(self.root)
+        print 'Bootstrapping Boost {} with {}'.format(self.version, ', '.join(BOOST_LIBS))
+        shell('./bootstrap.sh --with-libraries={}'.format(','.join(BOOST_LIBS)))
+        self.build_env.pop_dir()
+
+    def config_contents(self):
+        return """
+using darwin : {ios_sdk_version}~iphone
+: {xcode_root}/Toolchains/XcodeDefault.xctoolchain/usr/bin/{compiler} -arch armv7 -arch arm64 "-isysroot {xcode_root}/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS{ios_sdk_version}.sdk" -I{xcode_root}/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS{ios_sdk_version}.sdk/usr/include
+: <striper> <root>{xcode_root}/Platforms/iPhoneOS.platform/Developer
+: <architecture>arm <target-os>iphone
+;
+using darwin : {ios_sdk_version}~iphonesim
+: {xcode_root}/Toolchains/XcodeDefault.xctoolchain/usr/bin/{compiler} -arch i386 -arch x86_64 "-isysroot {xcode_root}/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator{ios_sdk_version}.sdk" -I{xcode_root}/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator{ios_sdk_version}.sdk/usr/include
+: <striper> <root>{xcode_root}/Platforms/iPhoneSimulator.platform/Developer
+: <architecture>x86 <target-os>iphone
+;
+using darwin : {osx_sdk_version}~osx
+: {xcode_root}/Toolchains/XcodeDefault.xctoolchain/usr/bin/{compiler} -arch i386 -arch x86_64 "-isysroot {xcode_root}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX{osx_sdk_version}.sdk" -I{xcode_root}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX{osx_sdk_version}.sdk/usr/include
+: <striper> <root>{xcode_root}/Platforms/MacOSX.platform/Developer
+: <architecture>x86 <target-os>darwin
+;
+        """.format(xcode_root=self.build_env.xcode_root,
+                   compiler=COMPILER,
+                   ios_sdk_version=self.build_env.ios_sdk_version,
+                   osx_sdk_version=self.build_env.osx_sdk_version)
+
+    def create_config(self):
+        if not os.path.isfile(self.config_path):
+            template_path = os.path.join(self.root, 'tools/build/example/user-config.jam')
+            if not os.path.isfile(template_path):
+                print 'File doesn\'t exist: {}'.format(template_path)
+                sys.exit(1)
+            shutil.copyfile(template_path, self.config_path)
+            with open(self.config_path, 'a') as f:
+                f.write(self.config_contents())
+
+    def setup(self):
+        self.download()
+        self.unpack()
+        self.invent_missing_headers()
+        self.bootstrap()
+        self.create_config()
+
+class BuildTask:
+    def __init__(self, build_env, boost_source, platform, target='stage'):
+        self.build_env = build_env
+        self.boost_source = boost_source
+        self.platform = platform
+        self.target = target
+        self.relative_build_dir = '{}-build'.format(platform)
+        self.build_dir = self.boost_source.resolve_path(self.relative_build_dir)
+        self.relative_stage_dir = os.path.join(self.relative_build_dir, 'stage')
+        self.stage_dir = self.boost_source.resolve_path(self.relative_stage_dir)
+        self.relative_prefix_dir = os.path.join(self.relative_build_dir, 'prefix')
+        self.prefix_dir = self.boost_source.resolve_path(self.relative_prefix_dir)
+
+    def common_build_args(self):
+        return [
+            '-j16',
+            '--build-dir={}'.format(self.relative_build_dir),
+            '--stage-dir={}'.format(self.relative_stage_dir),
+            '--prefix={}'.format(self.relative_prefix_dir),
+            'linkflags="-stdlib={}"'.format(STD_LIB),
+            'link=static',
+            'variant=release',
+            '-sBOOST_BUILD_USER_CONFIG={}'.format(self.boost_source.config_path),
+            ]
+
+    def ios_and_simulator_build_args(self):
+        return [
+            'target-os=iphone',
+            ]
+
+    def ios_build_args(self):
+        return [
+            'macosx-version=iphone-{}'.format(self.build_env.ios_sdk_version),
+            'toolset=darwin-{}~iphone'.format(self.build_env.ios_sdk_version),
+            'define=_LITTLE_ENDIAN',
+            'architecture=arm',
+            ]
+
+    def simulator_build_args(self):
+        return [
+            'macosx-version=iphonesim-{}'.format(self.build_env.ios_sdk_version),
+            'toolset=darwin-{}~iphonesim'.format(self.build_env.ios_sdk_version),
+            'architecture=x86',
+            ]
+
+    def osx_build_args(self):
+        return [
+            'target-os=darwin',
+            'macosx-version={}'.format(self.build_env.osx_sdk_version),
+            'toolset=darwin-{}~osx'.format(self.build_env.osx_sdk_version),
+            'architecture=x86',
+            ]
+
+    def common_cpp_flags(self):
+        return [
+            '-std={}'.format(CPP_STD),
+            '-stdlib={}'.format(STD_LIB),
+            '-fvisibility=hidden',
+            '-fvisibility-inlines-hidden',
+            '-fPIC', 
+            '-DBOOST_SP_USE_SPINLOCK',
+            ]
+
+    def ios_and_simulator_cpp_flags(self):
+        return [
+            '-miphoneos-version-min={}'.format(IOS_MIN_VERSION),
+            '-fembed-bitcode',
+            ]
+
+    def osx_cpp_flags(self):
+        return [
+            '-mmacosx-version-min={}'.format(OSX_MIN_VERSION),
+            ]
+
+    def cpp_flags(self):
+        flags = self.common_cpp_flags()
+        if self.platform in ('ios', 'simulator'):
+            flags.extend(self.ios_and_simulator_cpp_flags())
+        elif self.platform == 'osx':
+            flags.extend(self.osx_cpp_flags())
+        else:
+            print 'Unexpected platform: {}'.format(self.platform)
+            sys.exit(1)
+        return flags
+
+    def build_args(self):
+        args = self.common_build_args()
+        if self.platform in ('ios', 'simulator'):
+            args.extend(self.ios_and_simulator_build_args())
+            if self.platform == 'ios':
+                args.extend(self.ios_build_args())
+            else:
+                args.extend(self.simulator_build_args())
+        elif self.platform == 'osx':
+            args.extend(self.osx_build_args())
+        else:
+            print 'Unexpected platform: {}'.format(self.platform)
+            sys.exit(1)
+        flags = self.cpp_flags()
+        if flags and len(flags) > 0:
+            args.append('cxxflags="{}"'.format(' '.join(flags)))
+        args.append(self.target)
+        return args
+
+    def run(self):
+        self.build_env.push_dir(self.boost_source.root)
+        print 'Running {} target on {}'.format(self.target, self.platform)
+        shell('./b2 {}'.format(' '.join(self.build_args())))
+        self.build_env.pop_dir()
 
 def prepare():
     make_dir(OUTPUT_DIR_LIB)
     shutil.copyfile(BOOST_CONFIG_FILE, USER_CONFIG_FILE)
     with open(USER_CONFIG_FILE, 'a') as f:
         f.write(USER_CONFIG_FILE_CONTENTS)
-
-def bootstrap():
-    cwd = os.getcwd()
-    os.chdir(BOOST_ROOT)
-
-    print 'Bootstrapping with {}'.format(', '.join(BOOST_LIBS))
-    shell('./bootstrap.sh --with-libraries={}'.format(','.join(BOOST_LIBS)))
-
-    os.chdir(cwd)
-
-def build_args(platform, phase):
-    return (
-        '-j{}'.format(NUM_THREADS),
-        '--build-dir={}-build'.format(platform),
-        '-sBOOST_BUILD_USER_CONFIG={}'.format(USER_CONFIG_FILE),
-        '--stagedir={}-build/stage'.format(platform),
-        ('--prefix={}'.format(BUILD_IOS_PREFIX_DIR) if platform == 'iphone' else ''),
-        '--toolset=darwin-{}~{}'.format(IOS_SDK_VERSION, platform),
-        'cxxflags="-miphoneos-version-min={} -stdlib={} -fembed-bitcode"'.format(IOS_MIN_VERSION, STD_LIB),
-        'variant=release',
-        'linkflags="-stdlib={}"'.format(STD_LIB),
-        'architecture={}'.format('arm' if platform == 'iphone' else 'x86'),
-        'target-os=iphone',
-        'macosx-version={}-{}'.format(platform, IOS_SDK_VERSION),
-        ('define=_LITTLE_ENDIAN' if platform == 'iphone' else ''),
-        'link=static',
-        phase
-        )
 
 def platform_for_arch(arch):
     if arch in ('armv7', 'arm64'):
@@ -269,22 +463,7 @@ def sdk_for_platform(platform):
         return 'iphonesimulator'
     else:
         print 'Invalid platform: {}'.format(platform)
-        sys.exit(1)        
-
-def build():
-    cwd = os.getcwd()
-    os.chdir(BOOST_ROOT)
-
-    print 'Running bjam for iphone-build stage'
-    shell('./bjam {}'.format(' '.join(build_args('iphone', 'stage'))))
-
-    print 'Running bjam for iphone-build install'
-    shell('./bjam {}'.format(' '.join(build_args('iphone', 'install'))))
-
-    print 'Running bjam for iphonesim-build stage'
-    shell('./bjam {}'.format(' '.join(build_args('iphonesim', 'stage'))))
-
-    os.chdir(cwd)    
+        sys.exit(1)         
 
 def package():
     cwd = os.getcwd()
@@ -354,30 +533,33 @@ def bcp():
 #
 #-------------------------------------------------------------------------------
 
-def main():
-
-    # Remove any old build artifacts
-    clean()
-
+if __name__ == '__main__': 
+    
     # Prepare the build folder
-    make_dir(BUILD_DIR)
+    build_env = BuildEnv(os.path.join(os.getcwd(), 'build'))
+    build_env.prepare()
 
-    # Download and unpack
-    download_boost_tarball()
-    unpack_boost_tarball()
-    invent_missing_headers()
+    # Download and unpack the boost source
+    boost_source = BoostSource(build_env, BOOST_VERSION)
+    boost_source.setup()
 
-    # Configure and build
-    prepare()
-    bootstrap()
-    build()
+    # Define the targets for each platform
+    tasks = {
+        "ios": ['stage', 'install'],
+        "simulator": ['stage'],
+        "osx": ['stage'],
+    }
+    platforms = tasks.keys()
+
+    # Build each platform's targets
+    for platform in platforms:
+        for target in tasks[platform]:
+            build_task = BuildTask(build_env, boost_source, platform, target)
+            build_task.run()
 
     # Package and install
-    package()
-    bcp()
+    # package()
+    # bcp()
 
     # Remove build artifacts
-    clean()
-
-if __name__ == '__main__': 
-    main()
+    # clean()
